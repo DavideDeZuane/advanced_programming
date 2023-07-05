3d8c2ca4693bee653eaa9873de8310c542d58cd3
DavideDeZuane <s1092669@studenti.univpm.it>
2023-07-05 23:43:01 +0200

    added auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; 
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment } from 'src/env/env';
+import { LoginButtonComponent } from './auth/login';
+import { LogoutButtonComponent } from './auth/logout';
+import { AuthenticationButtonComponent } from './auth/auth-button.component';
+import { ProfileComponent } from './auth/profile.component.';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginButtonComponent,
+    LogoutButtonComponent,
+    AuthenticationButtonComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: environment.auth0.callbackURL
+      }
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/env/env.ts
@@ -0,0 +1,9 @@
+export const environment = {
+	production: false,
+	auth0: {
+	  domain: 'dev-mklwxkr2dddffknh.us.auth0.com',
+	  clientId: 'jhIOlWQCKWSwTzdPICAxjooQ2OkJLmXB',
+	  callbackURL: 'http://localhost:4200'
+	}
+  };
+  
