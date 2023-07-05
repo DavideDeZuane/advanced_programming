76bdf3f8406464b14d803cadd0d5af2fb8ab3095
DavideDeZuane <s1092669@studenti.univpm.it>
2023-07-05 19:07:56 +0200

    mongo connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 const app:Express = express()
 const port = 3000
 
-const mongoose = require('mongoose');
-
+const dbUri:string = 'mongodb://adprogramming:adprogramming@localhost:27017/admin' 
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
+app.get('/db', (req:Request, res:Response) => { 
+  mongoose.connect(dbUri);
+  res.send('provato');
+  })
+
 app.listen(port, () => {
-  console.log(mongoose.connect('mongodb://adprogramming:adprogramming@mongodb27017/test')
-  )
   console.log(`Example app listening on port ${port}`)
-})
+});
+
