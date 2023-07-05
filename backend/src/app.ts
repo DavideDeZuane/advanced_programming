5751071b4d6282bef83373e49f69ffd102dd0a59
Michele <mikdr51@gmail.com>
2023-07-05 18:12:21 +0200

    db connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,15 @@ import express, { Express, Request, Response } from 'express';
 const app:Express = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+
+
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
+  console.log(mongoose.connect('mongodb://adprogramming:adprogramming@mongodb27017/test')
+  )
   console.log(`Example app listening on port ${port}`)
 })
