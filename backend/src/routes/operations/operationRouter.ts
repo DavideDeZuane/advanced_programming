91032192b78c40b9700aa41ca7e51e6c10b8801d
Michele <mikdr51@gmail.com>
2023-07-07 09:59:12 +0200

    Add operations Router

---
+++ backend/src/routes/operations/operationRouter.ts
@@ -0,0 +1,35 @@
+import { Router, Request, Response } from 'express';
+const router: Router = Router();
+
+//##### GET METHOD ######
+
+//all operations
+router.get('/operations', (req: Request, res: Response) => {
+    //return all operations
+});
+
+//operation by id
+router.get('/operations/:id', (req, res) => {
+    const operationId = req.params.id; // Ottieni l'ID dell'operazione dai parametri della richiesta
+})
+
+//operation by client id
+router.get('/operations/:user_id', (req, res) => {
+    const clientId = req.params.user_id; // Ottieni l'ID del cliente dai parametri della richiesta
+})
+
+//operation by employee id
+router.get('/operations/:employee_id', (req, res) => {
+    const employee_id = req.params.employee_id; // Ottieni l'ID del employee dai parametri della richiesta
+})
+
+
+//##### POST METHOD ######
+
+//insert new operation
+router.post('/addSystem', (req: Request, res: Response) => {
+
+    res.status(201)
+});
+
+export default router;
