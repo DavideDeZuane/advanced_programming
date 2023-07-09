bfcc27e747d8b54af3ca79fd309f3a2da9c02d29
DavideDeZuane <s1092669@studenti.univpm.it>
2023-07-09 17:37:08 +0200

    update validator

--- backend/src/middlewares/validation.ts
+++ backend/src/middlewares/validation.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 
 const checkValidation = (req:Request, res:Response, next:NextFunction) => {
     let errors = validationResult(req);
-    (!errors.isEmpty()) ? next(new Error()) : next();
+    (!errors.isEmpty()) ? next(new Error()) : console.log('validazione superata'); next();
 }
 
 /* le funzioni di express-validate vanno usate qui e poi una volta applicate tutte va utilizzato validationResult in questo modo si ottiene il risultato*/
@@ -11,10 +11,10 @@ const checkValidation = (req:Request, res:Response, next:NextFunction) => {
 /* fare un array di validazione per ogni tipologia di post che viene effettuta */ 
 /* aggiungiamo quelli che sono i campi del client per esempio */
 const user_validation = [
-    body('first_name').trim().escape().isAlpha(),
-    body('last_name').trim().escape().isAlpha(),
-    body('birth_date').trim().isDate(),
-    body('fiscal_code').trim().isVAT('IT'),
+    body('firstName').trim().escape().isAlpha(),
+    body('lastName').trim().escape().isAlpha('it-IT', {ignore: ' '}),
+    body('birthDate').isISO8601().toDate(),
+    body('fiscalCode').trim().matches(/^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/),
     body('address').trim(),
     checkValidation
 ]
--- backend/src/model/Client.ts
+++ backend/src/model/Client.ts
@@ -10,38 +10,56 @@ export interface IClient extends Document {
   createdAt: Date;
 }
 
-//il pacchetto per la validazione del codice fiscale italiano
-//npm install codice-fiscale-js
-const CodiceFiscale = require('codice-fiscale-js');
+const validator = {
+  firstName: {
+    function: function(value:string) {return /^[a-zA-Z\s]{2,50}$/.test(value);},
+    message: 'Il nome non è valido. Deve contenere solo lettere, spazi, apostrofi o trattini e avere una lunghezza compresa tra 2 e 50 caratteri.' 
+  },
+  lastName: {
+    function: function(value:string) {return /^[a-zA-Z\s]{2,50}$/.test(value);},
+    message: 'Il cognome non è valido. Deve contenere solo lettere, spazi, apostrofi o trattini e avere una lunghezza compresa tra 2 e 50 caratteri.' 
+  },
+  fiscalCode: {
+    function: function(value:string) { return /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/.test(value);},
+    message: 'Il codice fiscale non è valido. Assicurati che sia nel formato corretto.'
+  },
+  birthDate: {
+    function:function (value: Date): Boolean { return value < new Date(); },
+    message: 'La Data di Nascita non è valida.',
+  }
+}
+
 
 const clientSchema: Schema<IClient> = new Schema<IClient>({
   firstName: {
     type: String,
-    match: [/^[a-zA-Z] + $/, 'Only letters'],
+    validate: {
+      validator: validator.firstName.function,
+      message: validator.firstName.message
+    },
     required: true
   },
   lastName: {
     type: String,
-    match: [/^[a-zA-Z] + $/, 'Only letters'],
+    validate: {
+      validator: validator.lastName.function,
+      message: validator.lastName.message
+    },
     required: true
   },
   birthDate: {
     type: Date,
     validate: {
-      validator: function (value: Date): Boolean {
-        return value < new Date();
-      },
-      message: 'Invalid date'
+      validator: validator.birthDate.function,
+      message: validator.birthDate.message
     },
     required: true
   },
   fiscalCode: {
     type: String,
     validate: {
-      validator: function (value: String): Boolean {
-        return CodiceFiscale.isValid(value);
-      },
-      message: 'Invalid CF',
+      validator: validator.fiscalCode.function,
+      message: validator.fiscalCode.message
     },
     required: true,
     unique: true
