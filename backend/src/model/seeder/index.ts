4716c6eea828e673edd265b3736e31ce794733f5
Michele <mikdr51@gmail.com>
2023-07-09 14:24:05 +0200

    Add insex seeder also in app.ts

--- backend/src/model/seeder/index.ts
+++ backend/src/model/seeder/index.ts
@@ -1,12 +1,23 @@
-import mongoose, { Document, Model, Schema } from 'mongoose';
-import Client, {IClient} from '../Client';
-import Component, {IComponent} from '../Component';
-import Device, {IDevice} from '../Device';
-import DevicePrototype, {IDevicePrototype} from '../DevicePrototype';
-import Employee, {IEmployee} from '../Employee';
-import File, {IFile} from '../File';
-import Operation, {IOperation} from '../Operation';
-import System, {ISystem} from '../System';
-import Version, {IVersion} from '../Version';
+import { seedClient } from './clientSeeder';
+import { seedComponent } from './componentSeeder';
+import { seedDevice } from './deviceSeeder';
+import { seedEmployee } from './employeeSeeder';
+import { seedFile } from './fileSeeder';
+import { seedOperation } from './operationSeeder';
+import { seedProto } from './prototypeDevSeeder';
+import { seedSystem } from './systemSeeder';
+import { seedVersion } from './versionSeeder';
 
+const seed = () => {
+    seedClient();
+    seedComponent();
+    seedDevice();
+    seedEmployee();
+    seedFile();
+    seedOperation();
+    seedProto();
+    seedSystem();
+    seedVersion();
+}
 
+export {seed}
