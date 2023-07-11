b940d81aee5652406f484523e71c1be22b8aaee9
DavideDeZuane <s1092669@studenti.univpm.it>
2023-07-11 10:33:03 +0200

    added catch

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,9 +1,9 @@
-import client_controller from "./client.controller";
+import client from "./client.controller";
 import component_controller from "./component.controller";
 import employee_controller from "./employee.controller";
 import prototype_controller from "./prototype.controller";
 import device_controller from "./device.controller";
 import system_controller from "./system.controller";
 
-export {client_controller, component_controller, employee_controller, prototype_controller, device_controller, system_controller}
+export {client, component_controller, employee_controller, prototype_controller, device_controller, system_controller}
 
