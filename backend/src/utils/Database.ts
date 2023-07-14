3cf92572b9bb4b10817ffb94d6c3358399119841
DavideDeZuane <s1092669@studenti.univpm.it>
2023-07-14 11:57:46 +0200

    update

--- backend/src/utils/Database.ts
+++ backend/src/utils/Database.ts
@@ -1,8 +1,9 @@
-import mongoose, { Connection } from "mongoose";
+import mongoose from "mongoose";
 import AppLogger from "./Logger";
 
-//da mettere in una variabile d'ambiente
-const dbUri:string='mongodb://adprogramming:adprogramming@mongodb:27017/adprogramming' 
+const dbUri:string = `mongodb://${process.env.MONGO_ROOT}:${process.env.MONGO_PASS}@mongodb:${process.env.PORT_MONGO}/${process.env.MONGO_DATABASE}` 
+                    || 'mongodb://adprogramming:adprogramming@mongodb:27017/adprogramming'
+                ;
 
 class DB {
     private static istance:DB;
@@ -11,9 +12,8 @@ class DB {
 
     private constructor(){ 
         this.logger = AppLogger.getInstance();
-
         this.connection= mongoose.connect(dbUri)
-            .then( () => this.logger.info('Connessione al Database MongoDB'))
+            .then( () => this.logger.info('Estabilished connectionion with Mongo Database'))
             .catch((e: mongoose.Error) => this.logger.error(`Connessione al Database fallita: ${e.message}`));
     }
 
