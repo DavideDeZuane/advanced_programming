64b7cee4af8d79b8cbea338353bc1051849a5d9f
DavideDeZuane <s1092669@studenti.univpm.it>
2023-07-15 11:27:58 +0200

    table clients

--- frontend/src/app/table/table.component.ts
+++ frontend/src/app/table/table.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 
 interface Response {
+  _id:string;
   firstName:string;
   lastName:string;
   fiscalCode: string;
   vatNumber: string;
   address: string;
+  createdAt:string;
 }
 
 class Client implements Response{
   
+  _id: string = '';
   firstName:string = '';
   lastName:string = '';
   fiscalCode:string = '';
-  vatNumber: string = ''
-  address: string = ''
+  vatNumber: string = '';
+  createdAt: string = '';
+  address: string = '';
 
   constructor(){}
 }
@@ -29,10 +34,20 @@ class Client implements Response{
 })
 export class TableComponent {
 
-  header:string[]  = [ 'Nome', 'Cognome', 'Codice Fiscale', 'Indirizzo', 'Partita IVA' ];
+  header:string[]  = ['Nome', 'Cognome', 'Codice Fiscale', 'Indirizzo', 'Partita IVA', 'Action' ]
+  icon = faEdit;
+  delete = faTrash;
+
+
   clients:Client[];
 
   constructor(private http: HttpClient, private toastr: ToastrService) { this.clients = []}
+
+  allowedProp(key:string):boolean {
+    if(key === '_id') return false;
+    if(key === 'birthDate') return false;
+    return true;
+  }
 
   ngOnInit(){
     this.http.get<Client[]>('http://localhost:3000/clients').subscribe(
